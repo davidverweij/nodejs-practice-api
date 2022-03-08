1f676ef67b27724f08695b8d947c513ec0a2aad7
David Verweij <davidverweij@gmail.com>
2022-03-08 15:29:20 +0000

    convert imports to ES6

--- src/task12.js
+++ src/task12.js
@@ -1,15 +1,15 @@
-const fs = require('fs');
-const csvtojson = require('csvtojson');
-const { pipeline } = require('stream');
+import { createReadStream, createWriteStream } from 'fs';
+import csv from 'csvtojson';
+import { pipeline } from 'stream';
 
 const inFile = './csv/task12.csv';
 const outFile = './csv/task12.txt';
 
 // read in csv, convert to json and write to text
 pipeline(
-    fs.createReadStream(inFile),
-    csvtojson(),
-    fs.createWriteStream(outFile),
+    createReadStream(inFile),
+    csv(),
+    createWriteStream(outFile),
     (error) => {
         if (error) {
             console.error(error);
