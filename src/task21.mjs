7c4cefbb2bbbe3ef861c6766685039b9cbf45555
David Verweij <davidverweij@gmail.com>
2022-03-09 14:19:57 +0000

    CRUD with soft-delete established

--- src/task21.mjs
+++ src/task21.mjs
@@ -4,7 +4,17 @@ import { v4 as uuid } from 'uuid';
 const app = express();
 const port = 3000;
 
+// In-service memory user collection
 const users = [];
+
+// helper method to filter soft-deleted users from the 'db'
+function findUserIndex(id) {
+    const index = users.findIndex(user => user.id === id)
+    if (index >= 0 && !users[index].isDeleted) {
+        return index;
+    }
+    return -1; // same return as not found by 'findIndex'
+}
 
 // type User = {
 //     id: string;
@@ -17,12 +27,17 @@ const users = [];
 // Config
 app.use(express.json())
 
+// TEST METHOD, GET LIST OF USERS!
+app.get('/all', (req, res) => {
+    res.json(users);
+});
+
 // Get user by ID
 app.get('/user/:id', (req, res) => {
     const id = req.params.id;
-    const user = users.find(user => user.id === id);
-    if (user) {
-        res.status(200).json(user);
+    const userIndex = findUserIndex(id);
+    if (userIndex >= 0) {
+        res.status(200).json(users[userIndex]);
     } else {
         res.status(404).json({ message: `User '${id}' was not found.` })
     }
@@ -39,24 +54,30 @@ app.post('/user', (req, res) => {
 // Update user by ID
 app.put('/user/:id', (req, res) => {
     const id = req.params.id;
-    const user = users.findIndex(user => user.id === id);
+    const userIndex = findUserIndex(id);
 
-    console.log(user);
-
-    if (user >= 0) {
+    if (userIndex >= 0) {
         const updateUser = req.body;
         updateUser.id = id;
-        users[user] = updateUser;
+        users[userIndex] = updateUser;
         res.status(204).send();
     } else {
         res.status(404).json({ message: `User '${id}' was not found.` })
     }
 });
 
-// TEST METHOD, GET LIST OF USERS!
-app.get('/all', (req, res) => {
-    res.json(users);
-});
+// Delete user by ID
+app.delete('/user/:id', (req, res) => {
+    const id = req.params.id;
+    const userIndex = findUserIndex(id);
 
+    if (userIndex >= 0) {
+        // users.splice(userIndex, 1)      // hard-delete option
+        users[userIndex].isDeleted = true; // soft-delete
+        res.status(204).send();
+    } else {
+        res.status(404).json({ message: `User '${id}' was not found.` })
+    }
+});
 
 app.listen(port, () => { console.log(`Server is listening on localhost:${port}`) });
