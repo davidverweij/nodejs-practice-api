4d18ba8cfed2f67f829f8d323367a2e50b2bf9d6
David Verweij <davidverweij@gmail.com>
2022-03-22 17:35:10 +0000

    add 'integration' tests without DB

--- tests/controllers/user.test.ts
+++ tests/controllers/user.test.ts
@@ -1,3 +1,6 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-return-assign */
+
 import supertest, { SuperTest, Test } from "supertest";
 import { Sequelize } from "sequelize";
 import jwt from "jsonwebtoken";
@@ -8,17 +11,23 @@ import { Group, GroupPermissions } from "../../src/models/group";
 import User from "../../src/models/user";
 import ApiUser from "../../src/models/apiUser";
 import { jwtSecret } from "../../src/config";
+import { logger, errorLogger } from "../../src/config/logger";
 
 describe("UserController", () => {
   let mockSequelize: Sequelize;
   let request: SuperTest<Test>;
 
-  let user: User;
-  let group: Group;
-  let apiUser: ApiUser;
+  logger.transports.forEach((t) => (t.silent = true));
+  errorLogger.transports.forEach((t) => (t.silent = true));
 
-  const userAtt = { login: "login1", password: "pass1", age: 1 };
+  const userAtt = {
+    id: "769ee62d-7ea6-473f-a289-0547f82fd5e7",
+    login: "login1",
+    password: "pass1",
+    age: 1,
+  };
   const groupAtt = {
+    id: "c2a8995b-9b10-440b-9755-335949d0631f",
     name: "admin",
     permissions: [
       GroupPermissions.DELETE,
@@ -27,11 +36,12 @@ describe("UserController", () => {
     ],
   };
   const apiUserAtt = {
+    id: "6db9a161-2e0b-498b-86c6-4ef63e5525d0",
     login: "apiuser1",
     password: "1234",
   };
   const apiUserAttSQL = {
-    login: apiUserAtt.login,
+    ...apiUserAtt,
     password: bcrypt.hashSync(apiUserAtt.password, bcrypt.genSaltSync(10)),
   };
 
@@ -47,41 +57,41 @@ describe("UserController", () => {
     request = supertest(app);
   });
 
-  beforeEach(async () => {
-    user = await User.create(userAtt);
-    apiUser = await ApiUser.create(apiUserAttSQL);
-    // group = await Group.create(groupAtt);
-    // user.addGroup(group);
-  });
-
   afterEach(async () => {
     jest.clearAllMocks();
-    await mockSequelize.truncate({ cascade: true }); // clear all inserted models
-  });
-
-  afterAll(async () => {
-    await mockSequelize.close();
   });
 
   describe("/all", () => {
     it("should 401 when no token is provded", async () => {
+      const spy = jest.spyOn(User, "findAll");
+
       const res = await request.get("/user/all");
 
       expect(res.status).toBe(401);
+      expect(spy).not.toBeCalled();
     });
     it("should 403 when token is invalid", async () => {
+      const spy = jest.spyOn(User, "findAll");
+
       const res = await request
         .get("/user/all")
         .set("x-access-token", "something-invalid");
 
       expect(res.status).toBe(403);
+      expect(spy).not.toBeCalled();
     });
     it("should return all users", async () => {
+      const spy = jest
+        .spyOn(User, "findAll")
+        .mockImplementation(async () => [new User(userAtt)]);
+
       const res = await request
         .get("/user/all")
         .set("x-access-token", jwt.sign(apiUserAtt, jwtSecret));
 
       expect(res.status).toBe(200);
+      expect(spy).toBeCalled();
+      expect(res.body[0].id).toEqual(userAtt.id);
     });
   });
 });
